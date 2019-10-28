06c3284d4bb2d08fcfd9b16c77bab65783c0d434
Alex Culeva <culevalex@gmail.com>
2019-10-28 11:50:08 +0200

    Initial Commit

---
+++ Figgy/AppDelegate.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ImageSearchBuilder.build())
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ Figgy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Figgy
+//
+//  Created by Alex Culeva on 10/28/19.
+//  Copyright © 2019 Alex Culeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
